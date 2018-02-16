880cbe24f99a30b8cfb19365dc8067084cb95731
Grzegorz Nowiński <Grzegorz Nowiski>
2018-02-16 20:24:34 +0100

    First version on shooting

--- js/Player.js
+++ js/Player.js
@@ -1,17 +1,24 @@
-var maxVelocity = 150;
+let maxVelocity = 150;
+let UP = -1;
+let DOWN = 1;
+let RIGHT = 1;
+let LEFT = -1;
+let NONE = 0;
 
 class Player {
     constructor(p_sprite) {
         this.sprite = p_sprite;
         this.sprite.anchor.setTo(0.5, 0.5);
-        this.hp = 100;
         this.sprite.animations.add('moveUp', [13, 14, 15, 12], 4, false);
         this.sprite.animations.add('moveDown', [1, 2, 3, 0], 4, false);
         this.sprite.animations.add('moveRight', [9, 10, 11, 8], 4, false);
         this.sprite.animations.add('moveLeft', [5, 6, 7, 4], 4, false);
         this.sprite.animations.add('moveRight', [9, 10, 11, 8], 4, false);
-        this.horizontalVelocity = 0;
-        this.verticalVelocity = 0;
+        this.moveDirectionX = NONE;
+        this.moveDirectionY = DOWN;
+        this.fireDirectionX = NONE;
+        this.fireDirectionY = DOWN;
+        this.hp = 100;
     }
 
     scalePlayer(p_firstMutlipler, p_secondMultipler) {
@@ -23,53 +30,62 @@ class Player {
     }
     
     reset() {
-        this.horizontalVelocity = 0;
-        this.verticalVelocity = 0;
+        this.directionX = NONE;
+        this.directionY = NONE;
     }
 
-    setDirectionUp() {
-        this.verticalVelocity = -maxVelocity;
-        this.sprite.animations.play('moveUp', 15);
+    setDirectionX(p_direction) {
+        this.directionX = p_direction;
     }
 
-    setDirectionDown() {
-        this.verticalVelocity = maxVelocity;
-        this.sprite.animations.play('moveDown', 15);
+    setDirectionY(p_direction) {
+        this.directionY = p_direction;
     }
-
-    setDirectionRight() {
-        this.horizontalVelocity = maxVelocity;
-        if (this.verticalVelocity == 0) {
-            this.sprite.animations.play('moveRight', 15);
-        }
-        else if (this.verticalVelocity > 0) {
-            this.sprite.animations.play('moveDown', 15);
-        }
-        else if (this.verticalVelocity < 0) {
-            this.sprite.animations.play('moveUp', 15);
-        }
+    
+    getFireDirectionX() {
+        return this.fireDirectionX;
     }
+    
+    getFireDirectionY() {
+        return this.fireDirectionY;
+    }
+    
+    playAnimation() {
+        if (this.directionY != NONE) {
+            if (this.directionY == UP) {
+                this.sprite.animations.play('moveUp', 15);
+                this.fireDirectionY = UP;
+            }
+            if (this.directionY == DOWN) {
+                this.sprite.animations.play('moveDown', 15);
+                this.fireDirectionY = DOWN;
+            }
+            this.fireDirectionX = NONE;
+        }
+        else {
+            if (this.directionX == RIGHT) {
+                this.sprite.animations.play('moveRight', 15);
+                this.fireDirectionX = RIGHT;
+                this.fireDirectionY = NONE;
+            }
+            if (this.directionX == LEFT) {
+                this.sprite.animations.play('moveLeft', 15);
+                this.fireDirectionX = LEFT;
+                this.fireDirectionY = NONE;
+            }
 
-    setDirectionLeft() {
-        this.horizontalVelocity = -maxVelocity;
-        if (this.verticalVelocity == 0) {
-            this.sprite.animations.play('moveLeft', 15);
-        }
-        else if (this.verticalVelocity > 0) {
-            this.sprite.animations.play('moveDown', 15);
-        }
-        else if (this.verticalVelocity < 0) {
-            this.sprite.animations.play('moveUp', 15);
         }
     }
     
     move() {
         var normalizeMultiper = 1;
-        if ( math.pow(this.horizontalVelocity, 2) + math.pow(this.verticalVelocity, 2) >= math.pow(maxVelocity, 2) * 2) {
+        var horizontalVelocity = this.directionX * maxVelocity;
+        var verticalVelocity = this.directionY * maxVelocity;
+        if ( math.pow(horizontalVelocity, 2) + math.pow(verticalVelocity, 2) >= math.pow(maxVelocity, 2) * 2) {
             normalizeMultiper = math.sqrt( math.pow(maxVelocity, 2) / 2 )/ maxVelocity;
         }
-        this.sprite.body.velocity.x = normalizeMultiper * this.horizontalVelocity;
-        this.sprite.body.velocity.y = normalizeMultiper * this.verticalVelocity;
+        this.sprite.body.velocity.x = normalizeMultiper * horizontalVelocity;
+        this.sprite.body.velocity.y = normalizeMultiper * verticalVelocity;
     }
     
     y() {
